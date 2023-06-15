7e3fe922a118708492ba396970e3d5749297a612
Snowsore <snow_sore@hotmail.com>
2023-06-16 01:45:56 +0800

    Add search bar

--- src/components/CSVTable.jsx
+++ src/components/CSVTable.jsx
@@ -1,7 +1,8 @@
 import { ProTable } from "@ant-design/pro-components";
 import { useCSVTableFormURL } from "../csv2obj";
-import { Input } from "antd";
+import { Input, Table } from "antd";
 import { InfoCircleOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
 
 export default function CSVTable(props) {
   const { url, onClick } = props;
@@ -9,8 +10,17 @@ export default function CSVTable(props) {
 
   if (!data.length) return <></>;
 
-  const headers = data[0];
-  const records = data.slice(1, data.length);
+  return <TableContent data={data} onClick={onClick} />;
+}
+
+const TableContent = (props) => {
+  const { data: rawData, onClick } = props;
+
+  const headers = rawData[0];
+  const records = rawData.slice(1, rawData.length);
+
+  const [data, setData] = useState(records);
+  const { Search } = Input;
 
   const expHeaders = ["Select", ...headers];
 
@@ -22,7 +32,7 @@ export default function CSVTable(props) {
     };
   });
 
-  const dataFormat = records.map((record, index) => {
+  const dataFormat = data.map((record, index) => {
     const temp = {
       Select: <ClickableIcon onClick={() => onClick?.(record)} />,
       key: index,
@@ -33,14 +43,43 @@ export default function CSVTable(props) {
     return temp;
   });
 
+  const handleSearch = (str) => {
+    const reg = new RegExp(
+      str.split(" ").map((s) => `(?=.*${s})`),
+      "i"
+    );
+    const filteredData = records.filter((record) => {
+      return JSON.stringify(record).match(reg);
+    });
+    setData(filteredData);
+  };
+
   const configs = {
     columns: columns,
     dataSource: dataFormat,
     search: false,
   };
 
-  return <ProTable {...configs}></ProTable>;
-}
+  const tableStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+    padding: "4px",
+  };
+
+  return (
+    <div style={tableStyle}>
+      <Search
+        placeholder="input search text"
+        allowClear
+        enterButton="Search"
+        size="large"
+        onSearch={handleSearch}
+      />
+      <ProTable {...configs}></ProTable>
+    </div>
+  );
+};
 
 const ClickableIcon = (props) => {
   const { onClick } = props;
