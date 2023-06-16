28273532dd3f6093ae27328db20496832e6d25b7
Snowsore <snow_sore@hotmail.com>
2023-06-16 21:22:29 +0800

    Improve csv datasets readbilyty, improve CSVTable code

--- src/csv2obj.js
+++ src/csv2obj.js
@@ -11,15 +11,37 @@ const useCSVFromURL = (url) => {
       const csv = await res.text();
       setData(readString(csv));
     };
+    console.log("f");
     fetchData();
-  }, []);
+  }, [url]);
 
   return data;
 };
 
 const useCSVTableFormURL = (url) => {
   const csv = useCSVFromURL(url);
-  return csv.data ?? [];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (csv.data) {
+      const headers = csv.data[0];
+      const records = csv.data.slice(1, csv.data.length);
+
+      headers[0] = "Index";
+
+      const dataFormat = records.map((record, index) => {
+        const temp = {};
+        record.forEach((value, index) => {
+          temp[headers[index]] = record[index];
+        });
+        return temp;
+      });
+
+      setData(dataFormat);
+    }
+  }, [csv]);
+
+  return data;
 };
 
 export { useCSVFromURL, useCSVTableFormURL };
