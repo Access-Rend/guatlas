1806b835b555582828b49de87ff5f5533f591159
Snowsore <snow_sore@hotmail.com>
2023-06-21 22:01:51 +0800

    Fix wrong api proxy port number

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 8888,
     proxy: {
       "/api": {
-        target: "http://localhost:51730",
+        target: "http://localhost:8090",
         changeOrigin: true,
         secure: false,
         ws: false,
