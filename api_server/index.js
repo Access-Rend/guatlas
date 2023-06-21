3796f385ff7bee2c674663f7b7e445b94bf77ad4
Snowsore <snow_sore@hotmail.com>
2023-06-21 20:32:44 +0800

    Change default port

--- api_server/index.js
+++ api_server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const fs = require("fs");
 const readdir = require("@jsdevtools/readdir-enhanced");
 const { dir } = require("console");
-const db_path = '../public/db/'
+const db_path = "../public/db/";
 // const csv = require("csv-parser");
 // const results = [];
 
@@ -24,21 +24,18 @@ const db_path = '../public/db/'
 //     console.log("Import done");
 //   });
 
-
-const tree_dir = (path = '', depth = 1) => {
-  let dir_list = fs.readdirSync(path)
-  if(depth <= 1)
-    return dir_list
+const tree_dir = (path = "", depth = 1) => {
+  let dir_list = fs.readdirSync(path);
+  if (depth <= 1) return dir_list;
 
   dir_list = dir_list.map((name, idx) => {
-    if(fs.statSync(path + '/' + name).isDirectory())
-      return {[name] : tree_dir(path + '/' + name, depth-1)}
-    else 
-      return name
-  })
+    if (fs.statSync(path + "/" + name).isDirectory())
+      return { [name]: tree_dir(path + "/" + name, depth - 1) };
+    else return name;
+  });
 
-  return {[path.split('/').filter(Boolean).pop()] : dir_list}
-}
+  return { [path.split("/").filter(Boolean).pop()]: dir_list };
+};
 
 function listDirectory(path, deep) {
   try {
@@ -56,20 +53,21 @@ function listDirectory(path, deep) {
   }
 }
 
-app.get("/api/tree/", (req,res) => {
-
-  let r = tree_dir(db_path + req.query['path'], req.query['depth'])
-  r = r[Object.keys(r)[0]]
-  r = Object.keys(r).map((key)=>{
-    return {[Object.keys(r[key])] : r[key][Object.keys(r[key])]}
-  })
-  res.json(r)
-})
+app.get("/api/tree/", (req, res) => {
+  let r = tree_dir(db_path + req.query["path"], req.query["depth"]);
+  r = r[Object.keys(r)[0]];
+  r = Object.keys(r).map((key) => {
+    return { [Object.keys(r[key])]: r[key][Object.keys(r[key])] };
+  });
+  res.json(r);
+});
 
 app.get("/api/db/*", (req, res) => {
   const path = `${db_path} / ${req.path.split("/").slice(3).join("/") + "/"}`;
   const deep = parseInt(req.query.deep);
   res.json(listDirectory(path, deep));
-})
+});
 
-app.listen(51730);
+app.listen(8890, () => {
+  console.log("Server start at 8090");
+});
